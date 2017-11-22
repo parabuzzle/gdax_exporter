4b4cddedb5f2ff40e1ae4d1031e68e13f1a2ed66
Mike Heijmans <parabuzzle@gmail.com>
2017-11-21 22:01:56 -0600

    switch to websockets api

--- gdax_exporter.py
+++ gdax_exporter.py
@@ -1,47 +1,82 @@
 from prometheus_client import start_http_server
 from prometheus_client import Gauge
+from prometheus_client import Counter
 from time import sleep
 import gdax
-
-
-public_client = gdax.PublicClient()
+import signal
 
 stats = {}
 
+class GracefulKiller:
+    kill_now = False
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
 
-def statit(stat, value):
+    def exit_gracefully(self,signum, frame):
+        self.kill_now = True
+
+class GdaxWebsocketClient(gdax.WebsocketClient):
+    def on_open(self):
+        self.url = "wss://ws-feed.gdax.com/"
+        self.products = ["BTC-USD", "ETH-USD", "LTC-USD"]
+
+    def on_message(self, msg):
+        process_tick(msg)
+
+    def on_close(self):
+        print("-- Goodbye! --")
+
+
+def statit(stat, value, type="gauge"):
     if stat in stats.keys():
         gauge = stats[stat]
     else:
-        gauge = Gauge(stat, '')
+        if type == "gauge":
+            gauge = Gauge(stat, '')
+        else:
+            gauge = Counter(stat, '')
         stats[stat] = gauge
+    if type == "gauge":
+        gauge.set(float(value))
+    else:
+        gauge.inc(float(value))
 
-    gauge.set(value)
-
-def spread(data):
-    ask = float(data['ask'])
-    bid = float(data['bid'])
-    return round(((ask - bid) / ask) * 100, 5)
-
-
-def process_currency(currency, data):
+def process_tick(msg):
+    currency = msg['product_id'].replace('-USD', '')
     metric_base = 'gdax_' + currency.lower() + '_'
+    if msg['type'] == 'received':
+        stat_received(msg, metric_base)
+    if msg['type'] == 'match':
+        stat_match(msg, metric_base)
 
-    statit(metric_base + 'bid', data['bid'])
-    statit(metric_base + 'volume', data['volume'])
-    statit(metric_base + 'ask', data['ask'])
-    statit(metric_base + 'price', data['price'])
-    statit(metric_base + 'spread', spread(data))
+
+def stat_received(msg, metric_base):
+    if 'price' not in msg:
+        return
+    if msg['side'] == 'buy':
+        statit(metric_base + 'bid', msg['price'])
+    if msg['side'] == 'sell':
+        statit(metric_base + 'ask', msg['price'])
+
+def stat_match(msg, metric_base):
+    statit(metric_base + 'volume', msg['size'], 'counter')
+    if 'price' not in msg:
+        return
+    statit(metric_base + 'price', msg['price'])
 
 
 start_http_server(8000)
+killer = GracefulKiller()
+
+wsClient = GdaxWebsocketClient()
+wsClient.start()
+print(wsClient.url, wsClient.products)
 
 while True:
-    btc = public_client.get_product_ticker(product_id='BTC-USD')
-    eth = public_client.get_product_ticker(product_id='ETH-USD')
-    ltc = public_client.get_product_ticker(product_id='LTC-USD')
-
-    process_currency('btc_usd', btc)
-    process_currency('eth_usd', eth)
-    process_currency('ltc_usd', ltc)
     sleep(1)
+    if killer.kill_now:
+        wsClient.close()
+        break
+
+exit(0)
